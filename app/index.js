65bbc553aaa60fdbd626b0029813db32e16991db
Heorhii Hryhorov <heorhiihryhorov@Heorhiis-MacBook-Pro.local>
2021-05-02 13:29:38 +0300

    added linters

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const app = express();
 
 app.listen('4000', () => {
-    console.log('Server started on port 4000');
+  /* eslint-disable no-console */
+  console.log('Server started on port 4000');
 });
