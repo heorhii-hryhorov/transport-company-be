73bdd1001d52f9a92ac16086386877bec534de6c
Heorhii Hryhorov <heorhiihryhorov@Heorhiis-MacBook-Pro.local>
2021-05-02 20:05:22 +0300

    added cors and body parser middleware

--- app/index.js
+++ app/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { PORT } from '../configs/config';
+import cors from 'cors';
+import { API_CLIENT, PORT } from '../configs/config';
+
+const corsOptions = {
+  origin: API_CLIENT,
+};
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 app.listen(PORT, () => {
-  /* eslint-disable no-console */
+  /* eslint-disable-next-line no-console */
   console.log(`Server started on port ${PORT}`);
 });
--- configs/config.js
+++ configs/config.js
@@ -2,3 +2,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT;
+export const API_CLIENT = process.env.API_CLIENT;
